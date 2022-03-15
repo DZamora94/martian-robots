2406464fcd4493156bd84fae75a0dfbd1aee9541
David Zamora <david.zamorasthaler.com>
2022-03-15 17:59:37 +0000

    feat(martian-form): create module

---
+++ src/app/martian-form/martian-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-martian-form',
+  templateUrl: './martian-form.component.html',
+  styleUrls: ['./martian-form.component.scss']
+})
+export class MartianFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/martian-form/martian-form.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MartianFormComponent } from './martian-form.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [MartianFormComponent],
+  declarations: [MartianFormComponent],
+})
+export class MartianFormModule {}
