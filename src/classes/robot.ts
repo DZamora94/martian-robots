f058097cc4f471eb37afdaf788da84d4b335b415
David Zamora <david.zamorasthaler.com>
2022-03-15 16:35:40 +0000

    feat(robot): create class with initial params and functions

---
+++ src/classes/robot.ts
@@ -0,0 +1,27 @@
+// Parameters: xCoordinate, yCoordinate, orientation (N,S,E,W)
+
+// Functions: turnLeft, turnRight, moveForward
+
+export type Orientation = 'N' | 'S' | 'E' | 'W';
+
+export class Rover {
+  public xCoordinate: number;
+  public yCoordinate: number;
+  public orientation: Orientation;
+
+  constructor(
+    xCoordinate: number,
+    yCoordinate: number,
+    orientation: Orientation
+  ) {
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    this.orientation = orientation;
+  }
+
+  public turnLeft() {}
+
+  public turnRight() {}
+
+  public moveForward() {}
+}
